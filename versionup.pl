c8548d3315b7ec5233e67595a48dab5339bc18ff
Ben Bullock <benkasminbullock@gmail.com>
2020-12-31 10:49:47 +0900

    Version up script sets date in Changes
    Ignore copied c/h files and XS output files in .gitignore

--- versionup.pl
+++ versionup.pl
@@ -1,28 +1,47 @@
 #!/home/ben/software/install/bin/perl
+
+# The CPAN perl-reversion script seems to be making a muddle of things
+# sometimes, and it doesn't edit Changes, so I've made my own script.
+
 use warnings;
 use strict;
 use utf8;
 use FindBin '$Bin';
 use File::Slurper qw!read_text write_text!;
+use Deploy 'make_date';
 
 my $version = '0.57_01';
 my $newversion = '0.57_02';
 
-my @files = qw!
-Changes
+my @pmfiles = qw!
 lib/JSON/Parse.pm
 lib/JSON/Tokenize.pm
 lib/JSON/Whitespace.pm
 !;
 
-for my $file (@files) {
+for my $file (@pmfiles) {
     my $bfile = "$Bin/$file";
     my $text = read_text ($bfile);
     if ($text =~ s/\Q$version\E/$newversion/g) {
 	print "$file OK\n";
 	write_text ($bfile, $text);
     }
+    elsif ($text =~ /\Q$newversion/) {
+	warn "$file already at $newversion";
+    }
     else {
 	warn "$file failed";
     }
 }
+
+my $date = make_date ('-');
+my $changes = "$Bin/Changes";
+my $text = read_text ($changes);
+if ($text =~ s/(\Q$version\E|\Q$newversion\E) ([0-9-]+)/$newversion $date/) {
+    write_text ($changes, $text);
+}
+else {
+    warn "$changes failed";
+}
+
+
